6d4abdd47e432f28b6733742d89d4812c1dd72e1
Abdulrahman-2005 <bodyazmy.new.2005@gmail.com>
2022-03-29 08:32:19 +0200

    fixing displaying ayahnumber less than the real one, fixed, added |last ayah| question

--- js/generators.js
+++ js/generators.js
@@ -25,7 +25,7 @@ function createNormalSession(surahRange = random(lvlMap[level][0], lvlMap[level]
 	return sess;
 }
 
-let questions = ["اكتب رقم الاية", "اكتب الاية", "اكتب اسم السورة", "اكتب الاية التالية"];
+let questions = ["اكتب رقم الاية", "اكتب الاية", "اكتب اسم السورة", "اكتب الاية التالية", "اكتب الاية السابقة"];
 
 function createDivSession(surahRange = random(lvlMap[level][0], lvlMap[level][1])) {
 	let sess = {};
@@ -35,30 +35,35 @@ function createDivSession(surahRange = random(lvlMap[level][0], lvlMap[level][1]
 		console.log(surahRange);
 		let randomSurahChoice = random(surahRange, 114).toString();
 		let choosenSurah = ALLSURAH[randomSurahChoice];
-		let choosenSurahName = SUOR[randomSurahChoice];
-		let randomAyahChoice = random(choosenSurah.length);
-		let choosenAyah = choosenSurah[randomAyahChoice-1 < 0 ? 0 : randomAyahChoice-1]
+		let choosenSurahName = SUOR[+randomSurahChoice];
+		let randomAyahChoice = random(0, choosenSurah.length);
+		let choosenAyah = choosenSurah[randomAyahChoice]
 		let qType = random(questions.length);
 		if (qType === 0) {
             quiz.description = questions[qType];
             quiz.question = choosenAyah;
-            quiz.answer = randomAyahChoice;
+            quiz.answer = randomAyahChoice+1;
             quiz.header = {infoos: 1, info: [`سورة (${choosenSurahName})`]};
 		} else if (qType === 1) {
-            quiz.header = {infoos: 2, info: [`اية رقم (${randomAyahChoice})`, `سورة (${choosenSurahName})`]};
+            quiz.header = {infoos: 2, info: [`اية رقم (${randomAyahChoice+1})`, `سورة (${choosenSurahName})`]};
             quiz.description = questions[qType];
             quiz.question = "___",
             quiz.answer = choosenAyah;
 		} else if (qType === 2) {
-            quiz.header = {infoos: 1, info:[`اية رقم (${randomAyahChoice})`]};
+            quiz.header = {infoos: 1, info:[`اية رقم (${randomAyahChoice+1})`]};
             quiz.description = questions[qType];
             quiz.question = choosenAyah;
             quiz.answer = choosenSurahName;
 		} else if (qType === 3) {
-			quiz.header = {infoos: 2, info:[`اية رقم (${randomAyahChoice})`, `سورة (${choosenSurahName})`]};
+			quiz.header = {infoos: 2, info:[`اية رقم (${randomAyahChoice+1})`, `سورة (${choosenSurahName})`]};
             quiz.description = questions[qType];
             quiz.question = choosenAyah;
             quiz.answer = choosenSurah[randomAyahChoice];
+		} else if (qType === 4) {
+			quiz.header = {infoos: 2, info:[`اية رقم (${randomAyahChoice+1})`, `سورة (${choosenSurahName})`]};
+            quiz.description = questions[qType];
+            quiz.question = choosenAyah;
+            quiz.answer = choosenSurah[randomAyahChoice-1];
 		}
         sess[i.toString()] = quiz;
         quiz = {}
